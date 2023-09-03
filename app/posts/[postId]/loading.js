ae6e93aa864e18f7e1163be3c8793eff1ad9ddf4
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2023-09-03 12:49:05 +0300

    first-next-app load

--- app/posts/[postId]/loading.js
+++ app/posts/[postId]/loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const loading = () => {
   return (
     <div>
-      <span class="loader">L &nbsp; ading</span>
+      <span class="">Loading</span>
     </div>
   );
 };
